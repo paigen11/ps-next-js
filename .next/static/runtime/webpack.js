e34a79718c2e2746ef7770f5bf65c0293958f117
Paige Niedringhaus <paigen11@gmail.com>
2020-01-15 14:24:09 -0500

    primer on next js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e29d63b2f49494127311";
+/******/ 	var hotCurrentHash = "8e426884ecceb5dd897f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
